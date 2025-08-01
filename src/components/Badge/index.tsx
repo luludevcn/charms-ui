4439f1365c8c9442d700a9c5cdaa4bae8c0cc458
luludev <523946878@qq.com>
2025-08-01 17:10:56 +0800

    Update index.tsx

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -5,7 +5,7 @@ export const Badge: React.FC<BadgeProps> = ({ ...props }) => (
 
     <div className={`w-fit shadow rounded text-sm font-mono overflow-hidden ${props.leftBg}`} >
         <a className="flex gap-1.5  cursor-pointer" href={props.href}>
-            <span className="px-1 py-1 text-white!">{props.title}</span><span className={`px-1 py-1 text-white! ${props.rightBg}`}>{props.count}</span>
+            <span className="px-1 py-1 !text-white">{props.title}</span><span className={`px-1 py-1 text-white! ${props.rightBg}`}>{props.count}</span>
         </a>
     </div>
 )
